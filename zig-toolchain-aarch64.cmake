a934ba80fafe57e82da8bcc36493758e6c45ebfd
Russell Greene <russell@shotover.com>
2023-10-22 19:40:31 -0600

    remove bin

--- zig-toolchain-aarch64.cmake
+++ zig-toolchain-aarch64.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_PROCESSOR "aarch64")
-set(CMAKE_C_COMPILER "${ZIG_PREFIX}/bin/zig" cc -target aarch64-linux-gnu.2.17)
-set(CMAKE_CXX_COMPILER "${ZIG_PREFIX}/bin/zig" c++ -target aarch64-linux-gnu.2.17)
+set(CMAKE_C_COMPILER "${ZIG_PREFIX}/zig" cc -target aarch64-linux-gnu.2.17)
+set(CMAKE_CXX_COMPILER "${ZIG_PREFIX}/zig" c++ -target aarch64-linux-gnu.2.17)
